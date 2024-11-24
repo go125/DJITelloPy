b296b3f6c3e94a18b6b7eb5b16f7e4f6965890ac
go125 <34290064+go125@users.noreply.github.com>
2024-11-24 12:55:53 +0900

    add: sleepy

--- examples/manual-control-pygame.py
+++ examples/manual-control-pygame.py
@@ -7,50 +7,32 @@ import time
 # Speed of the drone
 S = 60
 # Frames per second of the pygame window display
-# A low number also results in input lag, as input information is processed once per frame.
 FPS = 120
 
 
 class FrontEnd(object):
-    """ Maintains the Tello display and moves it through the keyboard keys.
-        Press escape key to quit.
-        The controls are:
-            - T: Takeoff
-            - L: Land
-            - Arrow keys: Forward, backward, left and right.
-            - A and D: Counter clockwise and clockwise rotations (yaw)
-            - W and S: Up and down.
-    """
-
     def __init__(self):
-        # Init pygame
         pygame.init()
-
-        # Creat pygame window
         pygame.display.set_caption("Tello video stream")
         self.screen = pygame.display.set_mode([960, 720])
 
-        # Init Tello object that interacts with the Tello drone
         self.tello = Tello()
 
-        # Drone velocities between -100~100
         self.for_back_velocity = 0
         self.left_right_velocity = 0
         self.up_down_velocity = 0
         self.yaw_velocity = 0
         self.speed = 10
-
         self.send_rc_control = False
 
-        # create update timer
+        self.blur_effect = False  # New: Track blur state
+
         pygame.time.set_timer(pygame.USEREVENT + 1, 1000 // FPS)
 
     def run(self):
-
         self.tello.connect()
         self.tello.set_speed(self.speed)
 
-        # In case streaming is on. This happens when we quit this program without the escape key.
         self.tello.streamoff()
         self.tello.streamon()
 
@@ -71,6 +53,8 @@ class FrontEnd(object):
                         self.keydown(event.key)
                 elif event.type == pygame.KEYUP:
                     self.keyup(event.key)
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    self.toggle_blur_effect(event.pos)  # New: Handle checkbox click
 
             if frame_read.stopped:
                 break
@@ -78,78 +62,89 @@ class FrontEnd(object):
             self.screen.fill([0, 0, 0])
 
             frame = frame_read.frame
-            # battery n. 电池
             text = "Battery: {}%".format(self.tello.get_battery())
             cv2.putText(frame, text, (5, 720 - 5),
-                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            if self.blur_effect:  # Apply blur if the effect is active
+                frame = cv2.GaussianBlur(frame, (15, 15), 0)
+
             frame = np.rot90(frame)
             frame = np.flipud(frame)
 
             frame = pygame.surfarray.make_surface(frame)
             self.screen.blit(frame, (0, 0))
-            pygame.display.update()
 
+            # Draw checkbox
+            self.draw_checkbox()
+
+            if self.blur_effect:  # Show "眠気" if the effect is active
+                font = pygame.font.Font(None, 74)
+                text_surface = font.render("Sleepy!", True, (255, 0, 0))
+                self.screen.blit(text_surface, (400, 300))
+
+            pygame.display.update()
             time.sleep(1 / FPS)
 
-        # Call it always before finishing. To deallocate resources.
         self.tello.end()
 
     def keydown(self, key):
-        """ Update velocities based on key pressed
-        Arguments:
-            key: pygame key
-        """
-        if key == pygame.K_UP:  # set forward velocity
+        if key == pygame.K_UP:
             self.for_back_velocity = S
-        elif key == pygame.K_DOWN:  # set backward velocity
+        elif key == pygame.K_DOWN:
             self.for_back_velocity = -S
-        elif key == pygame.K_LEFT:  # set left velocity
+        elif key == pygame.K_LEFT:
             self.left_right_velocity = -S
-        elif key == pygame.K_RIGHT:  # set right velocity
+        elif key == pygame.K_RIGHT:
             self.left_right_velocity = S
-        elif key == pygame.K_w:  # set up velocity
+        elif key == pygame.K_w:
             self.up_down_velocity = S
-        elif key == pygame.K_s:  # set down velocity
+        elif key == pygame.K_s:
             self.up_down_velocity = -S
-        elif key == pygame.K_a:  # set yaw counter clockwise velocity
+        elif key == pygame.K_a:
             self.yaw_velocity = -S
-        elif key == pygame.K_d:  # set yaw clockwise velocity
+        elif key == pygame.K_d:
             self.yaw_velocity = S
 
     def keyup(self, key):
-        """ Update velocities based on key released
-        Arguments:
-            key: pygame key
-        """
-        if key == pygame.K_UP or key == pygame.K_DOWN:  # set zero forward/backward velocity
+        if key in [pygame.K_UP, pygame.K_DOWN]:
             self.for_back_velocity = 0
-        elif key == pygame.K_LEFT or key == pygame.K_RIGHT:  # set zero left/right velocity
+        elif key in [pygame.K_LEFT, pygame.K_RIGHT]:
             self.left_right_velocity = 0
-        elif key == pygame.K_w or key == pygame.K_s:  # set zero up/down velocity
+        elif key in [pygame.K_w, pygame.K_s]:
             self.up_down_velocity = 0
-        elif key == pygame.K_a or key == pygame.K_d:  # set zero yaw velocity
+        elif key in [pygame.K_a, pygame.K_d]:
             self.yaw_velocity = 0
-        elif key == pygame.K_t:  # takeoff
+        elif key == pygame.K_t:
             self.tello.takeoff()
             self.send_rc_control = True
-        elif key == pygame.K_l:  # land
-            not self.tello.land()
+        elif key == pygame.K_l:
+            self.tello.land()
             self.send_rc_control = False
 
     def update(self):
-        """ Update routine. Send velocities to Tello.
-        """
         if self.send_rc_control:
-            self.tello.send_rc_control(self.left_right_velocity, self.for_back_velocity,
+            self.tello.send_rc_control(
+                self.left_right_velocity, self.for_back_velocity,
                 self.up_down_velocity, self.yaw_velocity)
+
+    def draw_checkbox(self):
+        """ Draw the checkbox on the screen """
+        checkbox_color = (0, 255, 0) if self.blur_effect else (255, 255, 255)
+        pygame.draw.rect(self.screen, checkbox_color, (10, 10, 20, 20), 0)  # Checkbox square
+        font = pygame.font.Font(None, 36)
+        text_surface = font.render("Blur Effect", True, (255, 255, 255))
+        self.screen.blit(text_surface, (40, 10))
+
+    def toggle_blur_effect(self, pos):
+        """ Toggle the blur effect when checkbox is clicked """
+        if 10 <= pos[0] <= 30 and 10 <= pos[1] <= 30:
+            self.blur_effect = not self.blur_effect
 
 
 def main():
     frontend = FrontEnd()
-
-    # run frontend
-
     frontend.run()
 
 
